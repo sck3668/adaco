d0f587161b98a47ba3b433ec3d8352e42bf41362
2 <2@DESKTOP-EI7A5I2>
2020-06-24 18:44:08 +0900

    2020-06-24 6:43 김지용 테스트 ㅊ추가함 adminTest

--- adaco/src/main/java/AdminTest.java
+++ adaco/src/main/java/AdminTest.java
@@ -1,9 +1,19 @@
+import org.junit.*;
 import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.test.context.*;
 import org.springframework.test.context.junit4.*;
+
+import com.icia.adaco.dao.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/**/*-context.xml")
 public class AdminTest {
-
+	@Autowired
+	AuthorityDao authorityDao;
+	
+	@Test
+	public void authorityTest() {
+		authorityDao.insert("sprng123", "ROLE_USER");
+	}
 }
