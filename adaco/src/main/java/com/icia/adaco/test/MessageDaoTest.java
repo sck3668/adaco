5c26b5aba74262e3c3232f0320e6cce3a793124f
Kown Hyeok Ji <Kown Hyeok Ji@DESKTOP-JV2QLHA>
2020-06-25 13:03:10 +0900

    혁지커밋

---
+++ adaco/src/main/java/com/icia/adaco/test/MessageDaoTest.java
@@ -0,0 +1,35 @@
+package com.icia.adaco.test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.time.*;
+
+import javax.inject.*;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+import com.icia.adaco.dao.*;
+import com.icia.adaco.entity.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/**/*-context.xml")
+public class MessageDaoTest {
+	@Inject
+	private MessageDao messageDao;
+	
+	//@Test
+	public void insertTest() {
+		Message message = Message.builder().mno(1).title("테스트").username("혁지").content("아아")
+				.sendId("gk").recipientId("아").msgCheck(0).writeDate(LocalDateTime.now()).build();
+				assertThat(messageDao.insert(message), is(1));
+				
+	}
+	@Test
+	public void findAllMessageTest() {
+		messageDao.findAllMessage();
+	}
+}
