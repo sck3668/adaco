9448230643c9ace84b47793e40a4869d87346d62
ICIA <ICIA@DESKTOP-D9CVRTU>
2020-06-25 20:26:01 +0900

    1. artMapper : 검색어로 작품 검색, 검색어에 맞는 작품 개수 추가
    3. artTest / insert, update, delete test 완료
    
    4. optionMapper 생성 및 내용 기재

--- adaco/src/main/java/com/icia/adaco/test/ArtDaoTest.java
+++ adaco/src/main/java/com/icia/adaco/test/ArtDaoTest.java
@@ -19,20 +19,33 @@ public class ArtDaoTest {
 	@Autowired
 	private ArtDao artdao;
 	
-	//상품 등록 테스트
+	//상품 등록 테스트 ok
 	//@Test
 	public void insertTest() {
-		Art art = Art.builder().artName("신상").price(1000).mainImg(true).artImg(false).stock(300).tag("검색").category("목걸이").shopno(1234).couriPrice(3000).returnAddress("배송지야").courier("대한통운").accumulated(500).artistno(123).build();
+		Art art = Art.builder().artName("신상").price(1000).mainImg(true).artImg(false).stock(300).tag("검색").category("카테고리다").shopno(1).couriPrice(3000).returnAddress("배송지야").courier("대한통운").accumulated(500).artistno(3).build();
 		assertThat(artdao.writeByArt(art), is(1));
+		/*for(int i=1; i<=10; i++) {
+			artdao.writeByArt(Art.builder().artName("신상").price(1000).mainImg(true).artImg(false).stock(300).tag("검색").category("카테고리다").shopno(1).couriPrice(3000).returnAddress("배송지야").courier("대한통운").accumulated(500).artistno(3).build());
+			
+			}	*/
 		System.out.println(art);
 	}
 	
-	//상품 수정 테스트
-	/*@Test
+	//상품 수정 테스트 ok
+	//@Test 
 	public void updateTest() {
-		Art art = Art.builder().artName("변경신상").price(2000).mainImg(true).artImg(artImg)build();
+		Art art = Art.builder().artno(27).artName("변경신상").price(2000).mainImg(true).artImg(true).stock(50).tag("태그얏").couriPrice(2500).returnAddress("배송지 업뎃").accumulated(1000).favorite(true).build();
 		artdao.updateByArt(art);
-	}*/
+	}
+	
+	//상품 삭제 테스트 ok
+	//@Test
+	public void deleteTest() {
+		assertThat(artdao.deleteByArt(26), is(1));
+	}
+	
+	
+	
 	
 	
 
