4c18c4b20804d765c861d7716a0c255fb5692be7
2 <2@DESKTOP-EI7A5I2>
2020-06-26 17:43:39 +0900

    2020-06-26 16:57 김지용
    1. option 테이블 안만들어짐
    	=> options 테이블로 추가하고 관계 설정함
    
    2. review 테이블 star 컬럼 데이터타입 NUMBER => NVARCHAR2로 변경
    	=> enum 호환 위해서 (나중에 별점 어떻게 해야할지 생각해야 할 듯)
    
    3. users 테이블에 check_code 컬럼 추가
    	=> 데이터 타입 char(10)
       users 테이블에 total_point 컬럼 추가
    	=> 데이터 타입 number(9)
    
    4. Notice 엔티티에 isImportant 추가
    	=> 데이터 타입 Boolean
    
    5. 기타 어드민 변경

--- adaco/src/main/java/com/icia/adaco/dao/AdminUserDao.java
+++ adaco/src/main/java/com/icia/adaco/dao/AdminUserDao.java
@@ -13,7 +13,21 @@ public class AdminUserDao {
 	@Autowired
 	private SqlSessionTemplate tpl;
 
-//	유저 목록 조회 (유저 이름으로 페이징) - 인덱스 추가하고 페이징 들어오면 테스트 해볼 것 
+//	유저 목록 카운팅
+	public int countByUser(String username) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("username", username);
+		return tpl.selectOne("adminUserMapper.countByUser", map);
+	}
+	
+//	작가 목록 카운팅	
+	public int countByArtist(String username) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("username", username);
+		return tpl.selectOne("adminUserMapper.countByArtist", map);
+	}
+	
+//	유저 목록 조회 (유저 이름으로 페이징)
 	public List<User> findAllByUser(int startRowNum, int endRowNum) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("startRowNum", startRowNum);
@@ -21,12 +35,16 @@ public class AdminUserDao {
 		return tpl.selectList("adminUserMapper.findAllByUser", map);
 	}
 	
-//	유저 검색 기능 (아직 안배움. index 사용해야하는데 어케함?)))))))))))))))))))))
-	public int countByUser(String username) {
-		return tpl.selectOne("adminUserMapper.countByUser", username);
+//	유저 검색하기
+	public List<User> findUserByKeyWord(int startRowNum, int endRowNum, String keyWord) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("startRowNum", startRowNum);
+		map.put("endRowNum", endRowNum);
+		map.put("keyWord", keyWord);
+		return tpl.selectList("adminUserMapper.findUserByKeyWord", map);
 	}
-
-//	작가 목록 조회 (작가 이름으로 페이징) - 인덱스 추가하고 페이징 들어오면 테스트 해보고, 아이디, 이름, 이메일 확인해야 하는데 작가 테이블 내에 없음. 조인 필요 개어려울듯
+	
+//	작가 목록 조회 (작가 이름으로 페이징)
 	public List<Artist> findAllByArtist(int startRowNum, int endRowNum) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("startRowNum", startRowNum);
@@ -34,16 +52,9 @@ public class AdminUserDao {
 		return tpl.selectList("adminUserMapper.findAllByArtist", map);
 	}
 	
-//	작가 검색 기능 (유저랑 작가랑 통합하면 좋은데 테이블이 따로라서 조인 필요 이거 됨?))))))))))))))))))
-	public int countByArtist(String username) {
-		return tpl.selectOne("adminUserMapper.countByArtist", username);
-	}
-	
-//	유저 블락 (판매자 유저) -
+//	유저 블락 (판매자 유저)
 	public int updateByUser(User user) {
 		return tpl.update("adminUserMapper.updateByUser", user);
 	}
-	
-	
 	
 }
