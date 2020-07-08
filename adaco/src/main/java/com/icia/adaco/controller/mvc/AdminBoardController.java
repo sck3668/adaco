97a7043fd23edd866ba34f351d882f457acb7107
2 <2@DESKTOP-EI7A5I2>
2020-07-08 16:11:13 +0900

    스프링 시큐리티 관련 로그아웃 안되던 문제 해결 	a태그 get -> post로 ajax처리

--- adaco/src/main/java/com/icia/adaco/controller/mvc/AdminBoardController.java
+++ adaco/src/main/java/com/icia/adaco/controller/mvc/AdminBoardController.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotNull;
 
 import org.apache.commons.lang3.math.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.security.access.annotation.*;
 import org.springframework.security.access.prepost.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -34,18 +35,19 @@ public class AdminBoardController {
 	ObjectMapper objectMapper;
 	
 	
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/")
 	public ModelAndView adminIndex() {
 		return new ModelAndView("admin/index").addObject("index", service.indexRead());
 	}
 	
-//	@PreAuthorize("isAuthenticated()")
-//	@Secured("ROLE_ADMIN")
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/report_list")
 	public ModelAndView reportList(@RequestParam(defaultValue = "1")int pageno) {
 		return new ModelAndView("admin/report/list").addObject("reportPage", service.reportList(pageno));
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/report_delete")
 	public String reportDelete(@RequestParam @NotNull String cnos) {
 		List<Integer> list = new ArrayList<>();
@@ -57,27 +59,28 @@ public class AdminBoardController {
 		return "redirect:/admin/report_list";
 	}
 	
+	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/admin/question_write")
 	public ModelAndView questionWrite() {
 		return new ModelAndView("admin/question/write");
 	}
 	
+	@PreAuthorize("isAuthenticated()")
 	@PostMapping("/admin/question_write")
 	public String questionWrite(Question question, Principal principal) {
 		String username = principal.getName();
 		return "redirect: /adaco/admin/question_read?qno="+service.questionWrite(question, username);
 	}
 	
-//	@Secured("ROLE_ADMIN")
-//	@PreAuthorize("isAuthenticated()")
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/question_list")
 	public ModelAndView questionList(@RequestParam(defaultValue = "1")int pageno, @Nullable String writer, @Nullable State searchType) {
 		return new ModelAndView("admin/question/list"
 				+ "").addObject("questionPage", service.questionList(pageno, writer, searchType));
 	}
 	
-//	@Secured("ROLE_ADMIN")
 //	@PreAuthorize("isAuthenticated()")
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/question_read")
 	public ModelAndView qusetionRead(@RequestParam(value = "qno")@NonNull Integer qno) throws JsonProcessingException {
 		ModelAndView mav = new ModelAndView("admin/question/read");
@@ -88,18 +91,20 @@ public class AdminBoardController {
 	}
 	
 //	@PreAuthorize("isAuthenticated()")
-//	@Secured("ROLE_ADMIN")
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/question_answer")
 	public String questionAnswer(Question question, Principal principal) {
 		service.questionAnswer(question);
 		return "redirect:/admin/question_read?qno="+question.getQno();
 	}
-	
+
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/notice_list")
 	public ModelAndView noticeList(@RequestParam(defaultValue = "1")int pageno, @Nullable Boolean isImportant) {
 		return new ModelAndView("admin/notice/list").addObject("noticePage", service.noticeList(pageno, isImportant));
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/notice_write")
 	public ModelAndView noticeWrite() {
 		return new ModelAndView("admin/notice/write");
@@ -114,7 +119,8 @@ public class AdminBoardController {
 		return mav;
 		
 	}
-	
+
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/notice_write")
 	public String write(AdminBoardDto.DtoForNoticeWrite dto) {
 		dto.setWriter("관리자");
@@ -128,33 +134,33 @@ public class AdminBoardController {
 		return "redirect:/";
 	}
 	
-//	@PreAuthorize("isAuthenticated()")
-//	@Secured("ROLE_ADMIN")
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/notice_update")
 	public String noticeUpdate(Notice notice) {
 		service.noticeUpdate(notice);
 		return "redirect:/admin/notice_read?noticeno="+notice.getNoticeno();
 	}
 
-	
+
 	@GetMapping("/user/faq_list")
 	public ModelAndView faqList() {
 		return new ModelAndView("admin/faq/list").addObject("faqList", service.faqList());
 	}
 	
-//	@PreAuthorize("isAuthenticated()")
-//	@Secured("ROLE_ADMIN")
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/admin/faq_write")
 	public ModelAndView faqWrite() {
 		return new ModelAndView("admin/faq/write");
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/faq_write")
 	public String faqWrite(FAQ faq) {
 		service.faqWrite(faq);
 		return "redirect:/user/faq_list";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/admin/faq_delete")
 	public String daqDelete(Integer faqno) {
 		service.faqDelete(faqno);
