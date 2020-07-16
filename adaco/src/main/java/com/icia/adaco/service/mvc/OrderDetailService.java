2c52295ee4529433327d295b49ba839cb76e8df7
sck36 <sck36@DESKTOP-B5S54T5>
2020-07-16 10:23:05 +0900

    결제

--- adaco/src/main/java/com/icia/adaco/service/mvc/OrderDetailService.java
+++ adaco/src/main/java/com/icia/adaco/service/mvc/OrderDetailService.java
@@ -27,27 +27,27 @@ public class OrderDetailService {
 	
 	// 결제하기
 	public int payment(OrderDto.DtoForAfter dto,String username) {
-		System.out.println("orderDto after dto ==="+dto);
+		//System.out.println("orderDto after dto ==="+dto);
 		OrderDetail orderDetail = modelMapper.map(dto,OrderDetail.class);
-		System.out.println("orderDetail=="+orderDetail);
+		//System.out.println("orderDetail=="+orderDetail);
 		int artno = dto.getArtno();
-		System.out.println("payment-artno==="+artno);
+		//System.out.println("payment-artno==="+artno);
 		Art art = artDao.readByArt(artno);
-		System.out.println("art=="+art);
+		//System.out.println("art=="+art);
 		Option option = optionDao.readByArtno(artno);
-		System.out.println("option=="+option);
+		//System.out.println("option=="+option);
 		Bag bag = bagDao.findByArtno(artno);
-		System.out.println("bag==="+bag);
+		//System.out.println("bag==="+bag);
 		User user = userDao.findByid(username);
-		System.out.println("user====="+user);
+		//System.out.println("user====="+user);
 		orderDetail.setArtName(art.getArtName()).setAddress(dto.getOriginalAddress());
 		orderDetail.setOptionName(option.getOptionName()).setOptionValue(option.getOptionValue());
 		orderDetail.setAmount(bag.getAmount()).setPrice(art.getPrice()).setEmail(user.getEmail());
 		orderDetail.setAddPoint((int) (art.getPrice()*0.01)).setState(State.답변대기);
 		//artName,optionName,optionValue,amount,price,email,tel,
 		//request,addPoint,postalcode,refundAccount,isShipping,state,address,recipient,
-		System.out.println("orderDetail11111======"+orderDetail);
-		
+		//System.out.println("orderDetail11111======"+orderDetail);
+		bagDao.deleteByBag(artno);
 		return orderDetailDao.Payment(orderDetail);
 	}
 	
